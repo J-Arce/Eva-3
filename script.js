73e303509574f3b10c4b42309c2ca887aceb2e81
J-Arce <pakkuzerox@gmail.com>
2023-05-29 01:34:29 -0400

    add local storage
    se agrego el sexto input y se guardan los datos en un Local Storage

--- script.js
+++ script.js
@@ -1,15 +1,28 @@
-const formulario = document.getElementById("formulario");
-
-formulario.addEventListener('submit',mostrarDatos)
-
-function mostrarDatos(a){
-    a.preventDefault();
-    const nombre = document.getElementById('nombre').value
-    const edad = document.getElementById('edad').value
-    const fnacimiento = document.getElementById('fnacimiento').value
-    const email = document.getElementById('email').value
-    const telefono = document.getElementById('telefono').value
-    const documentoRsh = document.getElementById('documentoRsh').value
-
-    console.log(nombre,edad,fnacimiento,email,telefono,documentoRsh)
+var registrar = () =>{
+    let eNombre = document.getElementById('nombre');
+    let eEdad = document.getElementById('edad');
+    let eFNacimiento = document.getElementById('fNacimiento');
+    let eEmail = document.getElementById('email');
+    let eTelefono = document.getElementById('telefono');
+    let eGenero = document.getElementById('genero');
+    let nombre = eNombre.value;
+    let edad = eEdad.value;
+    let fNacimiento = eFNacimiento.value;
+    let email = eEmail.value;
+    let telefono = eTelefono.value;
+    let genero = eGenero.value;
+    let alumno = {"nombre":nombre, "edad":edad, "fNacimiento":fNacimiento, "email":email, "telefono":telefono, "genero":genero};
+    let listaAlumnos = localStorage.getItem('alumnos');
+    let listaAntigua = JSON.parse(listaAlumnos);
+    if(listaAntigua == null){
+        listaNueva = [alumno];
+    }
+    else{
+        listaNueva = [...listaAntigua, alumno];
+    }
+    console.log(listaAntigua);
+    console.log(listaNueva);
+    localStorage.setItem('alumnos',JSON.stringify(listaNueva));
 }
+
+document.getElementById('btn').addEventListener('click', registrar)
