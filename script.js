9304ff8a452de8f6d4499744153a121b83150aba
J-Arce <pakkuzerox@gmail.com>
2023-06-06 02:27:27 -0400

    add tabla
    Se agrega una tabla con los datos ingresados al presionar el botón enviar y una alerta que muestra los valores guardados al presionar "Editar"

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 var registrar = () =>{
+    let econtenedorTabla = document.getElementById("contenedorTabla");
     let eNombre = document.getElementById('nombre');
     let eEdad = document.getElementById('edad');
     let eFNacimiento = document.getElementById('fNacimiento');
@@ -23,6 +24,30 @@ var registrar = () =>{
     console.log(listaAntigua);
     console.log(listaNueva);
     localStorage.setItem('alumnos',JSON.stringify(listaNueva));
+    render = "<table>"
+    render += "<tr><th>Nombre</th><th>Edad</th><th>Fecha Nacimiento</th><th>Email</th><th>Telefono</th><th>Genero</th><th>Accion</th></tr>"
+    for (let i = 0; i < listaNueva.length; i++) {
+        const element = listaNueva[i];
+        render += "<tr>"
+        render += "<td>"+element.nombre+"</td>"
+        render += "<td>"+element.edad+"</td>"
+        render += "<td>"+element.fNacimiento+"</td>"
+        render += "<td>"+element.email+"</td>"
+        render += "<td>"+element.telefono+"</td>"
+        render += "<td>"+element.genero+"</td>"
+        render += "<td>"
+        render += "<button id='btnEditar"+i+"'>Editar</button>"
+        render += "<button>Eliminar</button>"
+        render += "</td>"
+        render += "</tr>"
+    }
+    render += "</table>";
+    econtenedorTabla.innerHTML = render;
+    for (let i = 0; i < listaNueva.length; i++) {
+        var eBtn = document.getElementById("btnEditar"+i);
+        let element = listaNueva[i];
+        eBtn.addEventListener('click',()=>{alert("Hola "+element.nombre+" Edad: "+element.edad+" que nacio el "+element.fNacimiento+" que su email es: "+element.email+", su numero es: "+element.telefono+" y su genero es: "+element.genero)})
+    }
 }
 
 document.getElementById('btn').addEventListener('click', registrar)
